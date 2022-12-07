6bc8b5aba71efcf9f84e959b1672793fa61246fc
Antonio Alves <antonioaugusto.alves1981@gmail.com>
2022-12-07 14:39:07 -0300

    Criar script de inicialização do app

--- dist/src/index.js
+++ dist/src/index.js
@@ -1,13 +1,4 @@
 "use strict";
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
@@ -21,8 +12,8 @@ app.use(express_1.default.json());
 app.get("/home", (request, response) => {
     return response.status(200).json({ message: "Hello World" });
 });
-app.get("/users", (request, response) => __awaiter(void 0, void 0, void 0, function* () {
-    const users = yield prisma.user.findMany();
+app.get("/users", async (request, response) => {
+    const users = await prisma.user.findMany();
     return response.status(200).json(users);
-}));
+});
 app.listen(port, () => console.log("Server is running on port ", port));
